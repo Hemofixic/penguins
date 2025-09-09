742584a891a62e7c8c0d7d6699c8566ef304f926
Mariia Akhmetova <akhmetovamariyya@gmail.com>
2025-09-09 18:25:22 +0300

    боксплот код

--- penguins.R
+++ penguins.R
@@ -1,6 +1,6 @@
 data <- read.csv("raw/penguins.csv")
 x <- data$bill_length_mm
 
-png("pics/penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+png("pics/penguins-boxplot.png")
+boxplot(x)
 dev.off()
